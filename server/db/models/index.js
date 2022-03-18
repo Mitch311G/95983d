a5be11b5b4b73b6249a114c69714c417a412522d
Mitchell <mitchellwgardner@gmail.com>
2022-03-18 15:04:31 -0600

    Redesign DB: Users and Conversations are now many to many relationship

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,9 +4,10 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+// Create junction/join table 'UserConvo' to break up the many to many relationship b/t User and Conversation into two one to many relationships.
+User.belongsToMany(Conversation, { through: 'UserConvo' });
+Conversation.belongsToMany(User, { through: 'UserConvo' });
+
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
